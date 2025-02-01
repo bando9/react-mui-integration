a771b842a1d0e8109715cbf4693e61c93668d2de
bando9 <bandomega1@gmail.com>
2025-02-01 21:05:02 +0700

    feat: Add TextField component using Material UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,58 @@
-import { DatePicker, } from "@mui/x-date-pickers";
+import { DatePicker } from "@mui/x-date-pickers";
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+
+
+const currencies = [
+    {
+      value: 'All',
+      label: 'All',
+    },
+    {
+      value: 'EUR',
+      label: '€',
+    },
+    {
+      value: 'BTC',
+      label: '฿',
+    },
+    {
+      value: 'JPY',
+      label: '¥',
+    },
+  ];
 
 function Header(){
     return (
-        <div>
-            <h1>Header 1</h1>
-            <DatePicker label="Tanggal Lahir" />
+        <div style={{ display: 'flex', gap: '1rem' }} >
+            <DatePicker label="Auto date range" />
+            <TextField
+                id="outlined-select-currency"
+                select
+                label="Services"
+                defaultValue="All"
+                style={{ minWidth:100 }}
+                >
+                {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
+
+            <TextField
+                id="outlined-select-currency"
+                select
+                label="Posts"
+                defaultValue="All"
+                style={{ minWidth:100 }}
+                >
+                {currencies.map((option) => (
+                    <MenuItem key={option.value} value={option.value}>
+                    {option.label}
+                    </MenuItem>
+                ))}
+            </TextField>
         </div>
     )
 }
